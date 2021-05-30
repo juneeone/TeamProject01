c9270e738bb1376f54277c44c9b13d091b68bd23
juneeone <juneeone@naver.com>
2021-05-30 18:59:14 +0900

    5/30

--- Assets/스크립트/Player_Control.cs
+++ Assets/스크립트/Player_Control.cs
@@ -6,7 +6,7 @@ public class Player_Control : MonoBehaviour
 {
 
     private float Player_Speed = 0.05f;
-
+    public int score = 0;
 
     private void Player_Move()
     {
@@ -30,6 +30,16 @@ public class Player_Control : MonoBehaviour
             transform.Translate(Player_Speed, 0.0f, 0.0f);
         }
 
+    }
+
+    public void Score_one(Collider other)
+    {
+        if(other.gameObject.tag=="JEM")
+        {
+            Destroy(other.gameObject);
+            score += 100;
+            Debug.Log(score);
+        }    
     }
 
     // Start is called before the first frame update
