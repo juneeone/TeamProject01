27b76d6131492590771fa927d2fd789189a87aa1
juneeone <juneeone@naver.com>
2021-06-24 21:53:48 +0900

    FINAL

---
+++ Assets/C# Script/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    public string Over_Scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Game_Over();
+        }
+    }
+
+    void Game_Over()
+    {
+        SceneManager.LoadScene(Over_Scene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
